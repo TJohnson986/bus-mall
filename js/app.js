615d5f34ddca1f0a51864ff6000f36b65543e712
Tyler Johnson <tyler@csdconsultants.us>
2021-02-17 22:55:18 -0800

    added additional while loop to render product function, made changes to arrays to push and shift and make it so that any three images are not repeated

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@
 let totalClicks = 0;
 let allowedClicks = 25;
 let allProducts = [];
+let randomArray = [];
+let uniqueImageCount = 6;
 let firstImage = document.querySelector('main img:first-child');
 let secondImage = document.querySelector('main img:nth-child(2)');
 let thirdImage = document.querySelector('main img:nth-child(3)');
@@ -31,10 +33,10 @@ new Product('pen');
 new Product('pet-sweep');
 new Product('scissors');
 new Product('shark');
-new Product('sweep','png');
+new Product('sweep', 'png');
 new Product('tauntaun');
 new Product('unicorn');
-new Product('usb','gif');
+new Product('usb', 'gif');
 new Product('water-can');
 new Product('wine-glass');
 
@@ -43,10 +45,17 @@ function getRandomProduct() {
 }
 
 function renderProduct() {
-  let firstProductIndex = getRandomProduct();
-  let secondProductIndex = getRandomProduct();
-  let thirdProductIndex = getRandomProduct();
-  //add while loop here to not allow duplicates?
+  while (randomArray.length < uniqueImageCount) {
+    let randomProduct = getRandomProduct();
+    while (!randomArray.includes(randomProduct)) {
+      randomArray.push(randomProduct);
+    }
+  }
+  console.log(randomArray);
+  let firstProductIndex = randomArray.shift();
+  let secondProductIndex = randomArray.shift();
+  let thirdProductIndex = randomArray.shift();
+
 
   firstImage.src = allProducts[firstProductIndex].src;
   firstImage.title = allProducts[firstProductIndex].name;
@@ -61,37 +70,37 @@ function renderProduct() {
   allProducts[thirdProductIndex].views++;
 }
 
-function renderResults(){
+function renderResults() {
   let resultsList = document.querySelector('ul');
-  for (let i = 0; i < allProducts.length; i++){
+  for (let i = 0; i < allProducts.length; i++) {
     let li = document.createElement('li');
     li.textContent = `${allProducts[i].name} had ${allProducts[i].clicks} votes, and was seen ${allProducts[i].views} times.`;
     resultsList.appendChild(li);
   }
 }
 
-function handleClick(event){
-  if(event.target === myContainer){
+function handleClick(event) {
+  if (event.target === myContainer) {
     alert('Please click on an image');
   }
 
   totalClicks++;
   let productClicked = event.target.title;
 
-  for (let i = 0; i < allProducts.length; i++){
-    if (productClicked === allProducts[i].name){
+  for (let i = 0; i < allProducts.length; i++) {
+    if (productClicked === allProducts[i].name) {
       allProducts[i].clicks++;
     }
   }
 
   renderProduct();
-  if (totalClicks === allowedClicks){
-    myContainer.removeEventListener('click',handleClick);
+  if (totalClicks === allowedClicks) {
+    myContainer.removeEventListener('click', handleClick);
   }
 }
 
-function handleButtonClick(event){ //eslint-disable-line
-  if(totalClicks === allowedClicks){
+function handleButtonClick(event) { //eslint-disable-line
+  if (totalClicks === allowedClicks) {
     renderResults();
   }
 }
